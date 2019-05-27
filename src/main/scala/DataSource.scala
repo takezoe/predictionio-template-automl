00ede266129d1e96204210208cbf7208f8bd9194
Naoki Takezoe <takezoe@gmail.com>
2019-05-28 01:22:25 +0900

    Remove unnecessary lines

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -28,29 +28,8 @@ class DataSource(val dsp: DataSourceParams)
       entityType = Some("passenger"),
       eventNames = Some(List("titanic")))(sc)
 
-//   val passengersRDD = eventsRDD.map { event =>
-//      Passenger(
-//        event.entityId.toInt,
-//        event.properties.get[String]("survived").toInt,
-//        toOption(event.properties.get[String]("pClass")).map(_.toString),
-//        toOption(event.properties.get[String]("name")),
-//        toOption(event.properties.get[String]("sex")),
-//        toOption(event.properties.get[String]("age")).map(_.toDouble),
-//        toOption(event.properties.get[String]("sibSp")).map(_.toInt),
-//        toOption(event.properties.get[String]("parCh")).map(_.toInt),
-//        toOption(event.properties.get[String]("ticket")),
-//        toOption(event.properties.get[String]("fare")).map(_.toDouble),
-//        toOption(event.properties.get[String]("cabin")),
-//        toOption(event.properties.get[String]("embarked"))
-//      )
-//    }
-
     new TrainingData(eventsRDD)
   }
-
-//  private def toOption(s: String): Option[String] = {
-//    if(s.isEmpty) None else Some(s)
-//  }
 }
 
 class TrainingData(
@@ -60,18 +39,3 @@ class TrainingData(
     s"events: [${events.count()}] (${events.take(2).toList}...)"
   }
 }
-
-//case class Passenger(
-//  id: Int,
-//  survived: Int,
-//  pClass: Option[String],
-//  name: Option[String],
-//  sex: Option[String],
-//  age: Option[Double],
-//  sibSp: Option[Int],
-//  parCh: Option[Int],
-//  ticket: Option[String],
-//  fare: Option[Double],
-//  cabin: Option[String],
-//  embarked: Option[String]
-//)
--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -3,19 +3,6 @@ package org.example.vanilla
 import org.apache.predictionio.controller.EngineFactory
 import org.apache.predictionio.controller.Engine
 
-//case class Query(
-//  pClass: Option[Int],
-//  name: Option[String],
-//  sex: Option[String],
-//  age: Option[Double],
-//  sibSp: Option[Int],
-//  parCh: Option[Int],
-//  ticket: Option[String],
-//  fare: Option[Double],
-//  cabin: Option[String],
-//  embarked: Option[String]
-//) extends Serializable
-
 case class PredictedResult(survived: Double) extends Serializable
 
 object VanillaEngine extends EngineFactory {
